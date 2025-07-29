b92313882894600fd3d3b9b1499c1a9b78344d5b
Karine <karinedeveloper@gmail.com>
2025-07-28 21:47:02 -0300

    refactor: alterar exportações de tipos para declarações internas no arquivo Button.interface.ts

--- src/components/Button/Button.interface.ts
+++ src/components/Button/Button.interface.ts
@@ -1,8 +1,8 @@
 import type { ButtonHTMLAttributes, ReactNode } from 'react';
 
-export type ButtonSize = 'sm' | 'md' | 'lg' | 'w-full';
-export type ButtonFontSize = 'text-sm' | 'text-base' | 'text-lg' | 'text-xl';
-export type ButtonColor = 'primary' | 'secondary' | 'gost';
+type ButtonSize = 'sm' | 'md' | 'lg' | 'w-full';
+type ButtonFontSize = 'text-sm' | 'text-base' | 'text-lg' | 'text-xl';
+type ButtonColor = 'primary' | 'secondary' | 'gost';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
